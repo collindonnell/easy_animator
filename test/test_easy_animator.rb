f08604ba814f66da7f0f229fbe617738b60540f7
Collin Donnell <collin@waveform.fm>
2023-02-21 17:39:05 -0800

    Initial commit

---
+++ test/test_easy_animator.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestEasyAnimator < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::EasyAnimator::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
